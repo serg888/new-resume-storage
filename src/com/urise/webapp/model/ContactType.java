590278558dd4ead3b3fd4f0db262e296af5130c1
Сергей <serg888@mail.ru>
2016-10-10 21:20:48 +0700

    (HW7) исправление домашнего задания после разбора класс Resume в виде объектной модели

---
+++ src/com/urise/webapp/model/ContactType.java
@@ -0,0 +1,26 @@
+package com.urise.webapp.model;
+
+/**
+ * Created by Сергей on 10.10.2016.
+ */
+public enum ContactType {
+    PHONE("Тел."),
+    MOBILE("Мобильный"),
+    HOME_PHONE("Домашний тел."),
+    SKYPE("Skype"),
+    MAIL("Почта"),
+    LINKEDIN("Профиль LinkedIn"),
+    GITHUB("Профиль GitHub"),
+    STATCKOVERFLOW("Профиль Stackoverflow"),
+    HOME_PAGE("Домашняя страница");
+
+    private final String title;
+
+    ContactType(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
---
+++ src/com/urise/webapp/model/Section.java
@@ -0,0 +1,7 @@
+package com.urise.webapp.model;
+
+/**
+ * Created by Сергей on 10.10.2016.
+ */
+public abstract class Section {
+}
