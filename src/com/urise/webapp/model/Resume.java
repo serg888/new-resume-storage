f2df56299e9138dc28f8abab97a7eec84dff2874
Сергей <serg888@mail.ru>
2016-10-11 19:29:19 +0700

    -Тестирую выгрузку в GITHub

--- src/com/urise/webapp/model/Resume.java
+++ src/com/urise/webapp/model/Resume.java
@@ -10,7 +10,7 @@ import java.util.UUID;
  */
 public class Resume implements Comparable<Resume> {
 
-    //добавил для проверки работы GITHubjjj
+    //добавил для проверки работы GITHubjjjqq
     // Unique identifier
     private final String uuid;
     private final String fullName;
